ed86f0a8f737522edafa9f61733ce98c064daf97
Austin J.O. LeSure <austin.lesure@protonmail.com>
2017-06-20 09:07:14 -0700

    Now actually works

--- main.py
+++ main.py
@@ -14,4 +14,72 @@ def index():
     template = jinja_env.get_template('index.html')
     return template.render(title = 'User Signup')
 
+@app.route('/signup', methods=['POST'])
+def signup():
+    template = jinja_env.get_template('index.html')
+
+    username_error = ''
+    password_error = ''
+    password_verify_error = ''
+    user_email_error = ''
+
+    username = request.form['username']
+    if check_valid(username) == True:
+        good_username = username
+    else:
+        username = ''
+        username_error = 'Not a valid username.'
+    password = request.form['password']
+    if check_valid(password) == True:
+        good_password = password
+    else:
+        password_error = 'Not a valid password.'
+    password_verify = request.form['password_verify']
+    if password_verify != password:
+        password_verify_error = 'Passwords do not match.'
+    user_email = request.form['user_email']
+    if check_email(user_email) == False:
+        user_email_error = 'Not a valid email address.'
+    if username_error or password_error or password_verify_error or user_email_error:
+        return template.render(title = 'User Signup',username = username, username_error = username_error, password_error = password_error, password_verify_error = password_verify_error, user_email_error = user_email_error)
+    else:
+        return redirect('/greeting?username=' + good_username)
+
+@app.route('/greeting')
+def greeting():
+    username = request.args.get('username')
+    return '<h1>Hello, ' + username + '!</h1>'
+
+def check_valid(password):
+    for char in password:
+        if char == ' ':
+            return False
+    if len(password) < 3:
+        return False
+    elif len(password) > 20:
+        return False
+    else:
+        return True
+
+def check_email(user_email):
+    if user_email == '':
+        return True
+    at_count = 0
+    dot_count = 0
+    for char in user_email:
+        if char == ' ':
+            return False;
+        if char.isalnum() == False:
+            if char == '@':
+                at_count += 1
+            elif char == '.':
+                dot_count += 1
+            else:
+                return False
+    if dot_count > 2 or dot_count < 1:
+        return False
+    elif at_count != 1:
+        return False
+    return True
+
 app.run()
